d9aa897416bb0179017d560aa3852d6cd9aace23
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-06-15 21:34:55 +0300

    cleanup redundant files

---
+++ src/layouts/shipment/ShipmentItem.js
@@ -0,0 +1,31 @@
+ShipmentItem;
+
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+// const API_ENDPOINT = "https://my.api.mockaroo.com/shipments.json?key=5e0b62d0";
+
+// const ShipmentItem = () => {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [error, setError] = useState(null);
+//   const [data, setData] = useState(null);
+
+//   const fetchData = async () => {
+//     setIsLoading(true);
+//     await axios
+//       .get(API_ENDPOINT)
+//       .then((response) => {
+//         setData(response.json());
+//         setError(null);
+//         setIsLoading(false);
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   };
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+//   return { isLoading, error, data };
+// };
+
+// export default ShipmentItem;
